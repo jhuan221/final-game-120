a1c4669c7d66fe801081a03dda25a826b5504efe
Justin Murillo <murillo_justin@yahoo.com>
2022-05-26 12:12:42 -0700

    renamed and removed several scenes.

---
+++ src/scenes/Minigames/Sickness.js
@@ -0,0 +1,18 @@
+
+class S_Sickness extends Phaser.Scene {
+    constructor() {
+        super('s_sickness');
+    }
+
+    preload() {
+
+    }
+
+    create() {
+
+    }
+
+    update() {
+        
+    }
+}
